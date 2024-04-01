60f205e8abaa4364fabf5184f255a7453cecaa3e
Millenafernandes <millenaf66@gmail.com>
2024-04-01 19:32:11 -0300

    analise lexica

--- main.py
+++ main.py
@@ -1,25 +1,72 @@
 import ply.lex as lex
 
-# Lista de tokens
-tokens = ['INTEGER']
+tokens = [
+    'NUM_INT',
+    'NUM_DEC',
+    'ID',
+    'TEXTO',
+    'PALAVRA_RESERVADA',
+    'COMENTARIO',
+    'OPERADOR',
+    'SIMBOLO_ESPECIAL'
+]
 
-# Regras para tokens simples
-t_INTEGER = r'\d+'
+def t_NUM_INT(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
 
-# Ignorar caracteres em branco
+def t_NUM_DEC(t):
+    r'\d+\.\d+'
+    t.value = float(t.value)
+    return t
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = 'ID' if t.value not in reserved_words else 'PALAVRA_RESERVADA'
+    return t
+
+def t_TEXTO(t):
+    r'\".*?\"'
+    return t
+
+def t_COMENTARIO(t):
+    r'//.*'
+    return t
+
+def t_OPERADOR(t):
+    r'(\+|\-|\*|\/|\%|\&\&|\|\||\!|\>|\<|\>=|\<=|\!=|\==|\=)'
+    return t
+
+def t_SIMBOLO_ESPECIAL(t):
+    r'[\(\)\[\]\{\}\,\;]'
+    return t
+
+reserved_words = {
+    'int': 'PALAVRA_RESERVADA',
+    'float': 'PALAVRA_RESERVADA',
+    'char': 'PALAVRA_RESERVADA',
+    'boolean': 'PALAVRA_RESERVADA',
+    'void': 'PALAVRA_RESERVADA',
+    'if': 'PALAVRA_RESERVADA',
+    'else': 'PALAVRA_RESERVADA',
+    'for': 'PALAVRA_RESERVADA',
+    'while': 'PALAVRA_RESERVADA',
+    'scanf': 'PALAVRA_RESERVADA',
+    'println': 'PALAVRA_RESERVADA',
+    'main': 'PALAVRA_RESERVADA',
+    'return': 'PALAVRA_RESERVADA'
+}
+
 t_ignore = ' \t'
 
-# Tratamento de erro
 def t_error(t):
     print("Caractere ilegal: '%s'" % t.value[0])
     t.lexer.skip(1)
 
-# Criando o lexer
 lexer = lex.lex()
 
-# Testando o lexer
-lexer.input("123 456")
+lexer.input("123 123.456 variavel \"texto\" int if // comentario")
 
-# Iterando sobre os tokens
 for token in lexer:
     print(token)
